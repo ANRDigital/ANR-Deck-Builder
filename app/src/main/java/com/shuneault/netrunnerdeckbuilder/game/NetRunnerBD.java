44de8a42691d8c6f785b6adb8cee3e211bd18735
Sébastien Huneault <sebast331@gmail.com>
2016-04-07 07:48:32 -0400

    NetrunnerDB now uses HTTPS. Changed URL in file so images could download.

--- app/src/main/java/com/shuneault/netrunnerdeckbuilder/game/NetRunnerBD.java
+++ app/src/main/java/com/shuneault/netrunnerdeckbuilder/game/NetRunnerBD.java
@@ -2,8 +2,8 @@ package com.shuneault.netrunnerdeckbuilder.game;
 
 public class NetRunnerBD {
 
-    public static final String BASE_URL = "http://netrunnerdb.com";
+    public static final String BASE_URL = "https://netrunnerdb.com";
     public static final String URL_API_SEARCH = "/api/search/";
-    public static final String URL_GET_ALL_CARDS = "http://netrunnerdb.com/api/cards/?_locale=%s";
+    public static final String URL_GET_ALL_CARDS = "https://netrunnerdb.com/api/cards/?_locale=%s";
 
 }
