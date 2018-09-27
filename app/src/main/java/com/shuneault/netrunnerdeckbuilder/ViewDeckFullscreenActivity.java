660273ca7e27580e2e96c323ec4eaaea2f9808c6
Dan Johnson <danj3000@outlook.com>
2018-09-27 16:00:07 +0100

    updated to latest tooling

--- app/src/main/java/com/shuneault/netrunnerdeckbuilder/ViewDeckFullscreenActivity.java
+++ app/src/main/java/com/shuneault/netrunnerdeckbuilder/ViewDeckFullscreenActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/shuneault/netrunnerdeckbuilder/ViewDeckGridActivity.java
+++ app/src/main/java/com/shuneault/netrunnerdeckbuilder/ViewDeckGridActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
