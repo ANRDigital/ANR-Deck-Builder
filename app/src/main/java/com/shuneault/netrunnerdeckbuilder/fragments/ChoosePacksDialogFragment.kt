1f82b0c0fe75c6a413525abb28b83c8d5a612b17
Dan Johnson <danj3000@outlook.com>
2020-06-26 15:00:41 +0100

    fix white text on dialog by changing to materialalertdialogbuilder

--- app/src/main/java/com/shuneault/netrunnerdeckbuilder/fragments/ChoosePacksDialogFragment.kt
+++ app/src/main/java/com/shuneault/netrunnerdeckbuilder/fragments/ChoosePacksDialogFragment.kt
@@ -12,6 +12,7 @@ import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.shuneault.netrunnerdeckbuilder.R
 import com.shuneault.netrunnerdeckbuilder.db.CardRepository
 import com.shuneault.netrunnerdeckbuilder.fragments.dialog.PackListRecyclerViewAdapter
@@ -70,7 +71,7 @@ class ChoosePacksDialogFragment(val packFilter: ArrayList<String>, startFormat: 
 
         reloadPackList()
 
-        val builder = AlertDialog.Builder(activity)
+        val builder = MaterialAlertDialogBuilder(requireContext())
         builder.setTitle(R.string.choose_packs)
                 .setView(view)
                 // Set the action buttons
